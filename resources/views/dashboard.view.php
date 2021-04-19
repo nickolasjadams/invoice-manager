05772639849ede0482633c250097c6aae0af963b
Nick Adams <nickolas.j.adams@gmail.com>
2021-04-19 02:36:57 -0600

    delete mockup files

--- resources/views/dashboard.view.php
+++ resources/views/dashboard.view.php
@@ -24,6 +24,22 @@ include 'partials/dashboard/beginlayout.view.php';
 			echo "Upcoming: " . sizeof($upcoming);
 		}
 	?>
+
+	<!-- <div class="boxes">
+
+	<div class="box half">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/lMJvDi0KNlM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+
+	<div class="box quarter">
+		<img src="https://images.unsplash.com/photo-1492370284958-c20b15c692d2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1587&q=80" alt="">
+	</div>
+
+	<div class="box quarter">
+		Hello World
+	</div>
+
+	</div> -->
 	
 
 <?php
